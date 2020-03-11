f39ced3353f276e7a5c05b64222f7f6145d36d0a
MuslimMujahid <muslimmujahid1712@gmail.com>
2020-03-11 09:18:44 +0700

    buat loadFile.c milestone2

--- milestone2/build.sh
+++ milestone2/build.sh
@@ -1,10 +1,9 @@
 dd if=/dev/zero of=system.img bs=512 count=2880
 dd if=bootloader of=system.img bs=512 count=1 conv=notrunc
 dd if=map.img of=system.img bs=512 count=1 seek=1 conv=notrunc
-dd if=dir.img of=system.img bs=512 count=1 seek=2 conv=notrunc  
+dd if=files.img of=system.img bs=512 count=1 seek=2 conv=notrunc
+dd if=sectors.img of=system.img bs=512 count=1 seek=4 conv=notrunc  
 bcc -ansi -c -o kernel.o kernel.c
 nasm -f as86 kernel.asm -o kernel_asm.o
 ld86 -o kernel -d kernel.o kernel_asm.o
-dd if=kernel of=system.img bs=512 conv=notrunc seek=3
-./loadFile logo.txt
-./loadFile milestone1
+dd if=kernel of=system.img bs=512 conv=notrunc seek=5
