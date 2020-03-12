4d558311036a4dbf901dcd1592f8941774f15dea
MuslimMujahid <muslimmujahid1712@gmail.com>
2020-03-12 23:06:59 +0700

    shell bagian 4 dan 5 udah tapi belum dicek

--- milestone2/shell.c
+++ milestone2/shell.c
@@ -18,32 +18,40 @@
 #define USED 0xFF
 #define EMPTY 0x0
 #define DIR 0xFF
+#define BIN_INDEX 0x0
 
 // command type
 #define cd 1
 #define ls 2
 #define mkdir 3
+#define run 4
+#define bin 5
 
 // handler
+int getCommandHandler(char* command);
+void commandHandler(int type, char* input, char* curDirIndex, char* curPath);
+int commandCmp(char* str1, char* str2);
+void getArg(char* input, char* arg);
+int isDirExist(char* dirname, char curDirIndex);
+int isFileExist(char* dirname, char curDirIndex);
+char getDirIndexByName(char* dirname, char curDirIndex);
+char getParentIndexByCurIndex(char curDirIndex);
+
 int len(char* string);
 void pS(char *string, int newLine);
 void pI(int i, int newLine);
 void pC(char c, int newLine);
-int getCommandHandler(char* command);
-void commandHandler(int type, char* input, char* curDirIndex);
 void copy(char* src, char* dest);
 void copyRange(char* src, char* dest, int l, int h);
 int strCmp(char* str1, char* str2);
-int commandCmp(char* str1, char* str2);
-void getArg(char* input, char* arg);
-int isDirExist(char* dirname, char curDirIndex);
-char getDirIndexByName(char* dirname, char curDirIndex);
-char getParentIndexByCurIndex(char curDirIndex);
+void concat(char* src, char* dest);
 
 // command methods
 void _ls_(char curDirIndex);
 void _mkdir_(char* dirname, char parentIndex);
-void _cd_(char* dirname, char* curDirIndex);
+void _cd_(char* dirname, char* curDirIndex, char* curPath);
+void _run_(char* filename, char curDirIndex);
+void _bin_(char* filename);
 
 main()
 {
@@ -51,23 +59,179 @@ main()
     int command;
     char input[100];
     char argInput[100];
-    char curDir[100]; 
+    char curPath[100]; 
     char curDirIndex;
     char files[SECTOR_SIZE * 2];
 
     running = 1;
-    curDir[0] = '$'; 
+    curPath[0] = '$'; 
     curDirIndex = ROOT; // Begin in root
     while(running)
     {
         // Print current directory path
-        interrupt(0x21, 0x0, "$ ", 0, 0);
+        pS(curPath, FALSE); pC(' ', FALSE);
         interrupt(0x21, (0 << 8) | 0x1, input, 0, 0);
 
         // Read command
         command = getCommandHandler(input);
-        commandHandler(command, input, &curDirIndex);          
+        commandHandler(command, input, &curDirIndex, curPath);          
     }
+}
+
+// ================= HANDLER =====================
+int getCommandHandler(char* command)
+{
+    if (commandCmp(command, "cd")) return cd;
+    else if (commandCmp(command, "ls")) return ls;
+    else if (commandCmp(command, "mkdir")) return mkdir;
+    else if (command[0] == '.' && command[1] == '/') return run;
+    else return bin;
+    
+}
+void commandHandler(int type, char* input, char* curDirIndex, char* curPath)
+{
+    char arg[100];
+    getArg(input, arg);
+    switch (type)
+    {
+        case cd:
+            _cd_(arg, curDirIndex, curPath);
+            break;
+        case ls:
+            _ls_(*curDirIndex);
+            break;
+        case mkdir:
+            _mkdir_(arg, *curDirIndex);
+            break;
+        case run:
+            _run_(input + 2, *curDirIndex);
+            break;
+        case bin:
+            _bin_(input);
+        default:
+            break;
+    }
+}
+
+int commandCmp(char* str1, char* str2)
+{
+    int i;
+
+    i = 0;
+    while(str1[i] != SEPARATOR && str1[i] != '\0' && str2[i] != '\0')
+    {
+        if (str1[i] != str2[i])
+            break;
+        i++;
+    }
+    if (str1[i] == SEPARATOR) i--;
+    return (str1[i] == str2[i]);
+}
+
+void getArg(char* input, char* arg)
+{
+    int i = 0;
+    while (input[i] != SEPARATOR && input[i] != '\0')
+    {
+        i++;
+    }
+    copy(input + i + 1, arg);
+}
+
+int isDirExist(char* dirname, char curDirIndex)
+{
+    int i, j, filefound;
+    char files[SECTOR_SIZE * 2];
+
+    // Load files
+    interrupt(0x21, (0 << 8) | 0x02, files, FILES_SECTOR_1, 0);
+    interrupt(0x21, (0 << 8) | 0x02, files + SECTOR_SIZE, FILES_SECTOR_2, 0);
+
+    // Find filename in parentIndex
+    filefound = 0;
+    for (i = 0; i < SECTOR_SIZE * 2; i += FILES_COLUMNS)
+    {
+        if (files[i] == curDirIndex && files[i + 1] == DIR) // Check only dir
+        {
+            filefound = 1;
+            j = 0;
+            while (dirname[j] != '\0')
+            {
+                if (dirname[j] != files[i + NAME_OFFSET + j])
+                {
+                    filefound = 0;
+                    break;
+                }
+                j++;
+            }
+            if (filefound) return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+int isFileExist(char* dirname, char curDirIndex)
+{
+    int i, j, filefound;
+    char files[SECTOR_SIZE * 2];
+
+    // Load files
+    interrupt(0x21, (0 << 8) | 0x02, files, FILES_SECTOR_1, 0);
+    interrupt(0x21, (0 << 8) | 0x02, files + SECTOR_SIZE, FILES_SECTOR_2, 0);
+
+    // Find filename in parentIndex
+    filefound = 0;
+    for (i = 0; i < SECTOR_SIZE * 2; i += FILES_COLUMNS)
+    {
+        if (files[i] == curDirIndex && files[i + 1] != DIR) // Check only dir
+        {
+            filefound = 1;
+            j = 0;
+            while (dirname[j] != '\0')
+            {
+                if (dirname[j] != files[i + NAME_OFFSET + j])
+                {
+                    filefound = 0;
+                    break;
+                }
+                j++;
+            }
+            if (filefound) return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+char getDirIndexByName(char* dirname, char curDirIndex)
+{
+    int i, j, filefound;
+    char files[SECTOR_SIZE * 2];
+
+    // Load files
+    interrupt(0x21, (0 << 8) | 0x02, files, FILES_SECTOR_1, 0);
+    interrupt(0x21, (0 << 8) | 0x02, files + SECTOR_SIZE, FILES_SECTOR_2, 0);
+
+    for (i = 0; i < SECTOR_SIZE * 2; i += FILES_COLUMNS)
+    {
+        if (files[i] == curDirIndex && files[i + 1] == DIR)
+        {
+            if (strCmp(files + i + NAME_OFFSET, dirname))
+            {
+                return (i >> 0x4);
+            }
+        }
+    }
+}
+
+char getParentIndexByCurIndex(char curDirIndex)
+{
+    char files[SECTOR_SIZE * 2];
+
+    // Load files
+    interrupt(0x21, (0 << 8) | 0x02, files, FILES_SECTOR_1, 0);
+    interrupt(0x21, (0 << 8) | 0x02, files + SECTOR_SIZE, FILES_SECTOR_2, 0);
+
+    return files[curDirIndex * FILES_COLUMNS];
 }
 
 int len(char* string)
@@ -90,33 +254,6 @@ void pI(int i, int newLine)
 void pC(char c, int newLine)
 {
     interrupt(0x21, (0 << 8) | 0x8, c, newLine, 0);
-}
-
-int getCommandHandler(char* command)
-{
-    if (commandCmp(command, "cd")) return cd;
-    else if (commandCmp(command, "ls")) return ls;
-    else if (commandCmp(command, "mkdir")) return mkdir;
-}
-
-void commandHandler(int type, char* input, char* curDirIndex)
-{
-    char arg[100];
-    getArg(input, arg);
-    switch (type)
-    {
-        case cd:
-            _cd_(arg, curDirIndex);
-            break;
-        case ls:
-            _ls_(*curDirIndex);
-            break;
-        case mkdir:
-            _mkdir_(arg, *curDirIndex);
-            break;
-        default:
-            break;
-    }
 }
 
 void copy(char* src, char* dest)
@@ -163,21 +300,8 @@ int strCmp(char* str1, char* str2)
     return (str1[i] == str2[i]);
 }
 
-int commandCmp(char* str1, char* str2)
-{
-    int i;
 
-    i = 0;
-    while(str1[i] != SEPARATOR && str1[i] != '\0' && str2[i] != '\0')
-    {
-        if (str1[i] != str2[i])
-            break;
-        i++;
-    }
-    if (str1[i] == SEPARATOR) i--;
-    return (str1[i] == str2[i]);
-}
-
+// ==================== COMMAND METHODS ==================
 void _ls_(char curDirIndex)
 {
     int i, j;
@@ -233,17 +357,9 @@ void _mkdir_(char* dirname, char parentIndex)
     interrupt(0x21, (parentIndex << 8) | 0x5, 0, dirname, 0);
 }
 
-void getArg(char* input, char* arg)
-{
-    int i = 0;
-    while (input[i] != SEPARATOR && input[i] != '\0')
-    {
-        i++;
-    }
-    copy(input + i + 1, arg);
-}
 
-void _cd_(char* dirname, char* curDirIndex)
+
+void _cd_(char* dirname, char* curDirIndex, char* curPath)
 {
     int i, j;
     char nextDir[14];
@@ -267,6 +383,14 @@ void _cd_(char* dirname, char* curDirIndex)
 
             if (strCmp(nextDir, ".."))
             {
+                j = 0;
+                while (curPath[j] != '\0') j++;
+                while (curPath[j] != '/') 
+                {
+                    curPath[j] = EMPTY;
+                    j--;
+                }
+                curPath[j] = EMPTY;
                 tmpDirIndex = getParentIndexByCurIndex(tmpDirIndex);
             }
             else
@@ -280,6 +404,8 @@ void _cd_(char* dirname, char* curDirIndex)
                 {
                     tmpDirIndex = getDirIndexByName(nextDir, tmpDirIndex);
                 }
+                concat(curPath, "/");
+                concat(curPath, nextDir);
             }
         }
         if (dirname[i] != '\0') i++;
@@ -287,66 +413,36 @@ void _cd_(char* dirname, char* curDirIndex)
     *curDirIndex = tmpDirIndex; 
 }
 
-int isDirExist(char* dirname, char curDirIndex)
+void concat(char* src, char* dest)
 {
-    int i, j, filefound;
-    char files[SECTOR_SIZE * 2];
+    int i, j;
 
-    // Load files
-    interrupt(0x21, (0 << 8) | 0x02, files, FILES_SECTOR_1, 0);
-    interrupt(0x21, (0 << 8) | 0x02, files + SECTOR_SIZE, FILES_SECTOR_2, 0);
-
-    // Find filename in parentIndex
-    filefound = 0;
-    for (i = 0; i < SECTOR_SIZE * 2; i += FILES_COLUMNS)
+    i = 0;
+    j = 0;
+    while (src[i] != '\0') i++;
+    while (dest[j] != '\0')
     {
-        if (files[i] == curDirIndex && files[i + 1] == DIR) // Check only dir
-        {
-            filefound = 1;
-            j = 0;
-            while (dirname[j] != '\0')
-            {
-                if (dirname[j] != files[i + NAME_OFFSET + j])
-                {
-                    filefound = 0;
-                    break;
-                }
-                j++;
-            }
-            if (filefound) return TRUE;
-        }
-    }
-    return FALSE;
-}
-
-char getDirIndexByName(char* dirname, char curDirIndex)
-{
-    int i, j, filefound;
-    char files[SECTOR_SIZE * 2];
-
-    // Load files
-    interrupt(0x21, (0 << 8) | 0x02, files, FILES_SECTOR_1, 0);
-    interrupt(0x21, (0 << 8) | 0x02, files + SECTOR_SIZE, FILES_SECTOR_2, 0);
-
-    for (i = 0; i < SECTOR_SIZE * 2; i += FILES_COLUMNS)
-    {
-        if (files[i] == curDirIndex && files[i + 1] == DIR)
-        {
-            if (strCmp(files + i + NAME_OFFSET, dirname))
-            {
-                return (i >> 0x4);
-            }
-        }
+        src[i + j] = dest[j];
+        j++;
     }
 }
 
-char getParentIndexByCurIndex(char curDirIndex)
+void _run_(char* filename, char curDirIndex)
 {
-    char files[SECTOR_SIZE * 2];
+    if (!isFileExist(filename, curDirIndex))
+    {
+        pS("There is no such file!", TRUE);
+        return;
+    }
+    interrupt(0x21, (curDirIndex << 8) | 0x6, filename, 0x3000, 0);
+}
 
-    // Load files
-    interrupt(0x21, (0 << 8) | 0x02, files, FILES_SECTOR_1, 0);
-    interrupt(0x21, (0 << 8) | 0x02, files + SECTOR_SIZE, FILES_SECTOR_2, 0);
-
-    return files[curDirIndex * FILES_COLUMNS];
+void _bin_(char* filename)
+{
+    if (!isFileExist(filename, BIN_INDEX))
+    {
+        pS("Command not found!", TRUE);
+        return;
+    }
+    interrupt(0x21, (BIN_INDEX << 8) | 0x6, filename, 0x3000, 0);
 }
