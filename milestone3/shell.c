2a3ce9429eb5317d26c61a6b1decb575f9c68ced
MuslimMujahid <muslimmujahid1712@gmail.com>
2020-04-02 08:12:37 +0700

    menambah fungsi getCurDir

--- milestone3/shell.c
+++ milestone3/shell.c
@@ -729,3 +729,11 @@ void getArgv(int index, char* argv)
         }
     } 
 }
+
+void getCurDir(char* curdir)
+{
+    char args[SECTOR_SIZE];
+
+    interrupt(0x21, 0x02, args, ARGS_SECTOR, SECTOR_SIZE);
+    *curdir = args[0]; 
+}
